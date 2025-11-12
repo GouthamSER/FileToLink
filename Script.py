66eab97e0d21d6f8ad8a6705ab49a577cb60a4ec
Goutham Josh 么 <97418751+GouthamSER@users.noreply.github.com>
2025-11-12 21:43:32 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,12 +1,11 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+
 
 
 class script(object):
     START_TXT = """<b>Hello {} 👋, My Name Is <a href=https://t.me/{}>{}</a>
     
-✏️ I Am A Advance File Stream Bot With Multiple Player Support And Url Shortner. Best Ui Performance.
+✏️ I Am A Advance File Stream Bot With Multiple Player Support
+Best Ui Performance. ✅
 
 Now Send Me A Media To See Magic ✨</b>"""
 
