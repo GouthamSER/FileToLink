1192ba67b004db28d6f2c3cd1c39f4ee81baec30
GouthamJosh <96323721+GouthamJosh@users.noreply.github.com>
2025-11-12 17:51:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,8 +5,8 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'FileToLink')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '18979569'))
+API_HASH = environ.get('API_HASH', '45db354387b8122bdf6c1b0beef93743')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
@@ -24,7 +24,7 @@ URL = environ.get("URL", "")
 
 # Admins, Channels & Users
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6108995220').split()]
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
