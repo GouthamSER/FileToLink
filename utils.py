ac1b65a2fe258800bcf83877a6c28cc01ff7d5fa
Goutham Josh 么 <97418751+GouthamSER@users.noreply.github.com>
2026-02-24 23:06:31 +0530

    added th fn check_force_sub

--- utils.py
+++ utils.py
@@ -12,3 +12,14 @@ async def get_shortlink(link):
     shortzy = Shortzy(api_key=SHORTLINK_API, base_site=SHORTLINK_URL)
     link = await shortzy.convert(link)
     return link
+
+async def check_force_sub(client, user_id):
+    not_joined = []
+    for channel in FORCE_SUB_CHANNELS:
+        try:
+            await client.get_chat_member(channel, user_id)
+        except UserNotParticipant:
+            not_joined.append(channel)
+        except:
+            pass
+    return not_joined
