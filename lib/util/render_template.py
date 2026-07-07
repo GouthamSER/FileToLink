28d1c1c9996070ea1bb6aaac31f2779630f7b9fe
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2026-07-07 17:06:35 +0530

    Improve file name handling in render_page
    Updated file name handling in render_page function.

--- lib/util/render_template.py
+++ lib/util/render_template.py
@@ -16,19 +16,21 @@ async def render_page(id, secure_hash, src=None):
         logging.debug(f"Invalid hash for message with - ID {id}")
         raise InvalidHash
 
+    raw_name = file_data.file_name or f"file_{secure_hash}"
+
     src = urllib.parse.urljoin(
         URL,
-        f"{id}/{urllib.parse.quote_plus(file_data.file_name)}?hash={secure_hash}",
+        f"{id}/{urllib.parse.quote_plus(raw_name)}?hash={secure_hash}",
     )
 
-    tag = file_data.mime_type.split("/")[0].strip()
+    tag = (file_data.mime_type or "").split("/")[0].strip()
     file_size = humanbytes(file_data.file_size)
     template_file = "lib/template/req.html" if tag in ["video", "audio"] else "lib/template/dl.html"
 
     with open(template_file) as f:
         template = jinja2.Template(f.read())
 
-    file_name = file_data.file_name.replace("_", " ")
+    file_name = raw_name.replace("_", " ")
 
     return template.render(
         file_name=file_name,
