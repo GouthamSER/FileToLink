c9f938e619811a6333622a0f859133fc09381011
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2026-07-24 23:44:01 +0530

    Added plugins¹

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -30,6 +30,7 @@ class FileToLink(Client):
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             workers=50,
+            plugins={"root": "plugins"},
             sleep_threshold=5,
         )
 
